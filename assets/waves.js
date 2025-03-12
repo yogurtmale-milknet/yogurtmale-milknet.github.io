3d67e3dc65bcc66e370f0352eb92ed5b162106b4
Voxel <notvoxel@tuta.io>
2025-03-12 15:22:19 -0400

    Create waves.js

---
+++ assets/waves.js
@@ -0,0 +1,15 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.waves.min.js"></script>
+<script>
+VANTA.WAVES({
+  el: "#waves",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x1b1b1b
+})
+</script>
