996a3a518188d615257eac5d976b7feeb7e88604
Voxel <voxel@mailfence.com>
2025-04-13 11:12:38 -0400

    Create settings.js

---
+++ teletext/settings.js
@@ -0,0 +1,14 @@
+/* viewer settings */
+
+widescreen = false; /* emulate a 16:9 tv */
+prompt = true; /* user is prompted to click if autoOn or a channel parameter passed (required for media to play) */
+unevenFlashRatio = false; /* use a 2:1 ON/OFF ratio instead of 50% duty cycle in 1Hz flash */
+autoPowerOffTime = 15; /* minutes before powering off TV if no interaction, or zero to disable */
+
+/* set "tuning" for these channels when the page is loaded */
+localStorage.setItem("teletextViewer:channelID1","WyJ0ZWVmYXgiLCJ6eG5ldC5jby51ayIsIi93cy90ZWVmYXgiXQ==");
+localStorage.setItem("teletextViewer:channelID2","WyJubXMtY2VlZmF4IiwiaW50ZXJuYWwubmF0aGFubWVkaWFzZXJ2aWNlcy5jby51ayIsIi93ZWJzb2NrZXRzL2NlZWZheCJd");
+localStorage.setItem("teletextViewer:channelID3","WyJjaHVua3l0ZXh0IiwienhuZXQuY28udWsiLCIvd3MvY2h1bmt5dGV4dCJd");
+localStorage.setItem("teletextViewer:channelID4","WyJjaGFubmVsNC1jYXB0dXJlIiwienhuZXQuY28udWsiLCIvd3MvY2g0Il0=");
+localStorage.setItem("teletextViewer:channelID5","WyJ0ZXN0IiwienhuZXQuY28udWsiLCIvd3MvdGVzdCJd");
+localStorage.setItem("teletextViewer:channelID6","WyJzcGFyayIsInp4bmV0LmNvLnVrIiwiL3dzL3NwYXJrIl0=");
