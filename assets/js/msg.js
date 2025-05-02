170d5025bd4d9c97bb9e15c10078588d44048342
airfryers <voxel@mailfence.com>
2025-05-02 07:33:43 -0400

    Update msg.js

--- assets/js/msg.js
+++ assets/js/msg.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const messages = [
         "The Interstellar devs tried to sell my cousin fent",
         "What if Cool Math Games was called Cool Meth Games",
@@ -9,28 +9,27 @@ document.addEventListener("DOMContentLoaded", function() {
         "NEVER GOON",
         "5,000 visits and I drop my Adobe Enterprise login",
         "I'm Tim Cheese, and I killed John Pork. And this is my story. That day. The day I snuck up behind John. You all think you know the story. You don't",
-        "I am steve aggressive phonk gaming remix wait wrong tab",
         "Shoutout to everypizza.im for being british",
         "Voxel is eating a chicken sandwich come back later",
-        "The Ultraviolet devs tried to sell my cousin vitamin gummies"
+        "The Ultraviolet devs tried to sell my cousin vitamin gummies",
+        "https://youtu.be/76p9buRtpL4"
     ];
 
     const messageElement = document.getElementById("random-message");
 
     function updateMessage() {
-        const randomMessage = messages[Math.floor(Math.random() * messages.length)];
-        messageElement.textContent = randomMessage;
         messageElement.style.opacity = 0;
         messageElement.style.transform = "translateY(20px)";
-        messageElement.style.transition = "opacity 0.6s ease, transform 0.6s ease, text-shadow 0.6s ease";
-
+        
         setTimeout(() => {
+            const newMessage = messages[Math.floor(Math.random() * messages.length)];
+            messageElement.textContent = newMessage;
             messageElement.style.opacity = 1;
             messageElement.style.transform = "translateY(0)";
-            messageElement.style.textShadow = "0 0 10px black, 0 0 20px black, 0 0 30px black";
-        }, 100);
+        }, 1500);
     }
 
     updateMessage();
+
     setInterval(updateMessage, 6000);
 });
