fc3084198a78545a983175ba0c20a457fa4aeaca
airfryers <voxel@mailfence.com>
2025-05-09 14:03:50 +0000

    alphabetize games and i forgot to add the panic and cloak scripts to some games lmao

--- assets/js/msg.js
+++ assets/js/msg.js
@@ -9,8 +9,6 @@ document.addEventListener("DOMContentLoaded", function() {
         "NEVER GOON",
         "5,000 visits and I drop my Adobe Enterprise login",
         "I'm Tim Cheese, and I killed John Pork. And this is my story. That day. The day I snuck up behind John. You all think you know the story. You don't",
-        "Shoutout to everypizza.im for being british",
-        "Voxel is eating a chicken sandwich come back later",
         "The Ultraviolet devs tried to sell my cousin vitamin gummies"
     ];
 
