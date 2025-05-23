a29e87a0ccfe82e5d99c5430d6775c128226d398
Windex <voxel@mailfence.com>
2025-05-23 10:54:36 -0400

    Update cloak.js

--- assets/js/cloak.js
+++ assets/js/cloak.js
@@ -1,6 +1,6 @@
 document.addEventListener("keydown", function (event) {
     if (event.key === ".") {
-        let choice = prompt("CLOAK OPTIONS: Google Drive (g), Google Classroom (gc), Google Docs (gd), Clever (c), PowerSchool (p), Khan Academy (k), Desmos (d), Schoology (s), Cool Math Games (cmg), IXL (i), i-Ready Reading (ir), i-Ready Math (im)");
+        let choice = prompt("CLOAK OPTIONS: ALEKS (a), Clever (c), Cool Math Games (cl), Desmos (d), Edpuzzle (e), Google (gl), Google Drive (g), Google Classroom (gc), Google Docs (gd), Google Slides (gs), IXL (i), i-Ready Reading (ir), i-Ready Math (im), Khan Academy (k), PowerSchool (p), Schoology (s),");
 
         const options = {
             g: { title: "My Drive - Google Drive", icon: "/assets/img/drive.ico" },
@@ -11,10 +11,14 @@ document.addEventListener("keydown", function (event) {
             gd: { title: "Google Docs", icon: "/assets/img/docs.ico" },
             s: { title: "Schoology", icon: "/assets/img/schoology.ico" },
             d: { title: "Desmos | Scientific Calculator", icon: "/assets/img/desmos.ico" },
-            cmg: { title: "Cool Math Games - Free Online Games for Learning and Fun", icon: "/assets/img/coolmathgames.ico" },
+            cl: { title: "Cool Math Games - Free Online Games for Learning and Fun", icon: "/assets/img/coolmathgames.ico" },
             i: { title: "IXL | Math, Language Arts, Science, Social Studies, and Spanish", icon: "/assets/img/ixl.ico" },
             im: { title: "Math To Do, i-Ready", icon: "/assets/img/iready.ico" },
             ir: { title: "Reading To Do, i-Ready", icon: "/assets/img/iready.ico" },
+            gl: { title: "google classroom - Google Search", icon: "/assets/img/google.ico" },
+            gs: { title: "Google Slides", icon: "/assets/img/slides.ico" },
+            a: { title: "ALEKS - Home", icon: "/assets/img/aleks.ico" },
+            e: { title: "Edpuzzle", icon: "/assets/img/edpuzzle.ico" },
         };
 
         if (options[choice]) {
